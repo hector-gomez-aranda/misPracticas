8dda5e37dc2f869d75a7fdb4cded236e6ec8f084
Unknown <hector.gomez1997@gmail.com>
2019-03-19 16:33:04 -0600

    programa de entrada de datos 2
    programa de entrada 2

--- entrada.py
+++ entrada.py
@@ -1,5 +1,3 @@
 entrada = input("Ingresa algo: ")
-apellido = input("Ingresa un apellido: ")
-edad = input("Ingresa tu edad: ")
 
 print(entrada)
---
+++ entradaPersona.py
@@ -0,0 +1,10 @@
+# nombre = input("Ingresar nombre: ")
+# apellido = input("Ingresa un apellido: ")
+# edad = input("Ingresa tu edad: ")
+#
+# print(f"Bienvenido {nombre} {apellido}, su edad es : {edad}") #la f nos premite capturar las variables y concatenarlas
+#print(f"{2+2}")
+num1 =  float(input("Ingresa un numero: "))
+num2 = float(input("Ingresa otro numero: "))
+resultado = num1 + num2
+print(f"El resultado es: {resultado}")
