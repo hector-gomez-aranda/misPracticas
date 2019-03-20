95d1c2536d05f491c4fa916270388da85d7ee9ce
Unknown <hector.gomez1997@gmail.com>
2019-03-19 18:12:36 -0600

    Programa ciclos y condiciones
    programa ciclos y condiciones

---
+++ ciclos.py
@@ -0,0 +1,13 @@
+a = 0
+b = 100
+while(a<b):
+    #print(a)
+    a+=10
+for x in range(1,100,2): #inicia en uno cuando llegue antes de 100 y que aumente de 2 en 2
+    print(x)
+#ciclo for each
+lista = ["manzana", "pera", "platano"]
+for elemento in lista:
+    if(elemento!="manzana"):
+        print(elemento)
+print(len(lista))
---
+++ condicionales.py
@@ -0,0 +1,8 @@
+a = 5
+
+if(a>10):
+    print("a es mayor a 10")
+elif(a<0):
+    print("El numero es negativo")
+else:
+    print("El numero es menor a 10")
